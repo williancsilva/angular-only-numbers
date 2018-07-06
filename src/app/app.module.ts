1ae32000335a3ae860ab94d840e63dafd01ffba7
Willian Cândido <williancsilva@gmail.com>
2018-07-05 21:09:17 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NumberOnlyDirective } from './number.directive';
+
+import {Ng2BRPipesModule} from 'ng2-brpipes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberOnlyDirective 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2BRPipesModule,
+    FormsModule      
   ],
   providers: [],
   bootstrap: [AppComponent]
